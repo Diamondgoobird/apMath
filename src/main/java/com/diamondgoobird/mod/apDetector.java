348b72a299463083c93d023cda3147a77d0e2f96
Diamondgoobird <birdsplaymc2@gmail.com>
2021-08-10 15:52:19 -0700

    Using regex to get numbers instead of the other way that took me 3 hours to make
    title

--- src/main/java/com/diamondgoobird/mod/apDetector.java
+++ src/main/java/com/diamondgoobird/mod/apDetector.java
@@ -14,89 +14,53 @@ public class apDetector {
 	
 	@SubscribeEvent
 	public void getNumbers(ClientChatReceivedEvent event) {
+		if (event.message.getUnformattedText().contains("a>>") && event.message.getUnformattedText().contains("Progress")) {	
 		apMath.Config.load();
 		apMath.toggle = apMath.Config.getBoolean("toggle", "values", true, "Toggles whether the mod is enabled");
 		if (apMath.toggle == false) {
 			
 		}
 		else {
-		String message = event.message.getUnformattedText();
-		String num1 = "";
-		String num2 = "";
-		String withoutNumbers = "";
+		String num1;
+		String num2;
+		String pattern = "(?<numbers>\\d+)";
+		Pattern p = Pattern.compile(pattern);
+		Matcher m = p.matcher(event.message.getFormattedText());
+		int x = 0;
+		String[] q = new String[2];
+		try {
+			m.find();
+			q[0] = m.group("numbers");
+			m.find();
+			m.find();
+			q[1] = m.group("numbers");
+		}
+		catch (Exception oop) {
+			oop.printStackTrace();
+		}
 		int number1;
 		int number2;
-		boolean number = false;
-		int[] e = new int[message.length()];
-		int funnycharacter = 0;
-		char[] u = EnumChatFormatting.RESET.toString().toCharArray();
-		
-		if (message.contains("a>>")) {
-			char[] characters = message.toCharArray();
-			int x = 0;
-			int z = -1;
-			while (x < message.length()) {
-				if (characters[x] == ':') {
-					int xd = 0;
-					while (x > xd) {
-						String a = 
-						withoutNumbers = withoutNumbers + characters[xd];
-						xd++;
-					}
-					withoutNumbers = withoutNumbers + ":";
-					z = x + 3;
-				}
-				else if (characters[x] == '/') {
-					number = true;
-				}
-				else if (characters[x] == u[0] && z != -1) {
-					funnycharacter++;
-				}
-				
-				else {
-					if (z != -1) {
-						String a = "";
-						a = a + characters[x];
-						boolean fail = false;
-						try {
-							//System.out.println("Converting " + a + " to integer");
-							e[x] = Integer.parseInt(a);
-						}
-						catch (Exception jhjhj) {
-							fail = true;
-						}
-						if (fail == false) {
-							if (number == true) {
-								num2 = num2 + characters[x];
-							}
-							else {
-								if (funnycharacter > 1) {
-									
-								}
-								else {
-									num1 = num1 + characters[x];
-								}
-							}
-						}
-					}
-				}
-				x++;
-			}
+		num1 = q[0];
+		num2 = q[1];
 			if (num1 != null && num2 != null) {
 				number1 = Integer.parseInt(num1);
 				number2 = Integer.parseInt(num2);
 				System.out.println("I got numbers " + num1 + " and " + num2);
-				event.setCanceled(true);
+				//String replacing = "\u00A7" + "r" + "\u00A7" + "a" +number1 + "\u00A7" + "r" + "\u00A7" + "7/" + "\u00A7" + "r" + "\u00A7" + "a" + number2 + "\u00A7" + "r" + "\u00A7" + "r";
+				String replacing = "\u00A7r\u00A7a" + number1 + "\u00A7r\u00A77/\u00A7r\u00A7a" + number2 + "\u00A7r";
+				System.out.println("Replacing: " + replacing);
+				String replacement;
 				if (apMath.both == false) {
 					int difference = number2 - number1;
-					event.message = new ChatComponentText(event.message.getFormattedText().replace(num1 + "\u00A7" + "7/" + "\u00A7" + "a" + num2, "\u00A7" + "a" + difference + " left."));
-					Minecraft.getMinecraft().thePlayer.addChatMessage(event.message);
+					event.message = new ChatComponentText(event.message.getFormattedText().replace(replacing, "\u00A7" + "a" + difference + " left."));
+					Minecraft.getMinecraft().thePlayer.addChatComponentMessage(event.message);
 				}
 				else {
 					int difference = number2 - number1;
-					event.message = new ChatComponentText(event.message.getFormattedText().replace(num1 + "\u00A7" + "7/" + "\u00A7" + "a" + num2, "\u00A7" + "a" + difference + " left. " + "\u00A7" + "a" + "(" + num1 + "\u00A7" + "7/" + "\u00A7" + "a" + num2 + ")"));
-					Minecraft.getMinecraft().thePlayer.addChatMessage(event.message);
+					event.message = new ChatComponentText(event.message.getFormattedText().replace(replacing, "\u00A7" + "a" + difference + " left. " + "\u00A7" + "a" + "(" + num1 + "\u00A7" + "7/" + "\u00A7" + "a" + num2 + ")"));
+					Minecraft.getMinecraft().thePlayer.addChatComponentMessage(event.message);
 				}
+				event.setCanceled(true);
 			}
 			}
 		}
